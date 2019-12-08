6b31100e8a2e5e2cca36cc71cf2a1bf4ea812dec
yozian <yozian@hotmail.com>
2019-12-08 22:42:54 +0800

    add null check for enty

--- src/Yozian.Validation/Validation.cs
+++ src/Yozian.Validation/Validation.cs
@@ -7,6 +7,11 @@ namespace Yozian.Validation
         public static Validator<T> Entry<T>(T @this)
             where T : class
         {
+            if (null == @this)
+            {
+                throw new ArgumentException("Validation object cant NOT be NULL");
+            }
+
             return new Validator<T>(@this);
         }
     }
